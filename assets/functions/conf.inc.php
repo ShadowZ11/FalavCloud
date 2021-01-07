fd95c9a1a1c09a06580dd8c996235dd21b038e3e
ZAYNEB <zaynab.ntlm@gmail.com>
2021-01-07 21:28:38 +0100

    DBScript

--- assets/functions/conf.inc.php
+++ assets/functions/conf.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-	define("DBHOST", "ec2-54-170-100-209.eu-west-1.compute.amazonaws.com");
-	define("DBUSER", "hozulksfraxzkj");
-	define("DBPWD", "986e08deaf22c0d169e85031ec4273537ef16a34fa4e330c9e43e991504da882");
-	define("DBNAME", "dcohtn1ve5fs1u");
-	define("DBPORT", "5432");
+	define("DBHOST", "localhost");
+	define("DBUSER", "root");
+	define("DBPWD", "");
+	define("DBNAME", "pa");
+	define("DBPORT", "3308");
 
 	define("SALT", "kal25gghfq");
