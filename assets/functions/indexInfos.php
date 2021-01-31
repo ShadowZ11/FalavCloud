db7f007f4bc18242770fe9e2b221517eb64df0b2
ZAYNEB <zaynab.ntlm@gmail.com>
2021-01-31 21:27:34 +0100

    indexinfo

--- assets/functions/indexInfos.php
+++ assets/functions/indexInfos.php
@@ -1,28 +1,10 @@
 <?php
 	require_once("functions.php");
 
-	
-			
+
+
 	$connect = connectFalaDB();
 
-	
-	$sqlNews = $connect->query("SELECT ID_NEWS, DATE_NEWS, TITRE, PREVIEW, PHOTO_PREVIEW, ID_USERS FROM NEWS ORDER BY DATE_NEWS DESC");
+
+	$sqlNews = $connect->query("SELECT id_news, date_news, titre, preview, photo_preview, id_users FROM news ORDER BY date_news DESC");
 	$news = $sqlNews->fetchAll();
-
-
-	$sqlNewAlbums = $connect->query("SELECT ID_ALBUM, TITRE, PHOTO_ALBUM, FEAT FROM ALBUM ORDER BY DATE_SORTIE DESC");
-	$newAlbums = $sqlNewAlbums->fetchAll();
-
-
-	$sqlNewClip = $connect->query("SELECT LIEN_CLIP FROM MUSIQUE ORDER BY DATE_SORTIE DESC");
-	$newClip = $sqlNewClip->fetchAll();
-
-
-	$sqlRanking = $connect->query("SELECT ID_MUSIQUE, TITRE, NOMBRE_ECOUTE, PHOTO_MUSIQUE, FEAT FROM MUSIQUE ORDER BY NOMBRE_ECOUTE DESC");
-	$ranking = $sqlRanking->fetchAll();
-
-
-		
-			
-	
-		
