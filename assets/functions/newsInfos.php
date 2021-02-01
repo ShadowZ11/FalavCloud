121f474f1461aebaff43b748252cf7b9fc13b2f4
DregerTi <72619834+DregerTi@users.noreply.github.com>
2021-02-01 04:40:12 +0100

    Update newsInfos.php

--- assets/functions/newsInfos.php
+++ assets/functions/newsInfos.php
@@ -8,8 +8,8 @@
 		$sqlInfoNews = $connect->query("SELECT date_news, titre, preview, photo_preview, contenu, id_users FROM news WHERE id_news ='".$_GET["id_news"]."'");
 		$infoNews = $sqlInfoNews->fetch();
 
-		$sqlAuthNews = $connect->query("SELECT nom, prenom FROM users WHERE id_users ='".$infoNews["id_users"]."'");
-		$authNews = $sqlAuthNews->fetch();
+		//$sqlAuthNews = $connect->query("SELECT nom, prenom FROM users WHERE id_users ='".$infoNews["id_users"]."'");
+		//$authNews = $sqlAuthNews->fetch();
 							
 	}else{
 		header("location:../../unknownAlbum.php");
